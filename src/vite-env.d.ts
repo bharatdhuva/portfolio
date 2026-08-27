1c58897824195d142738e806159a6a7609317ee9
Bharat Dhuva <bharatdhuva27@gmail.com>
2026-08-27 16:32:31 +0530

    chore: initialize project with React, Vite, TypeScript, and Tailwind CSS configuration

---
+++ src/vite-env.d.ts
@@ -0,0 +1,35 @@
+/// <reference types="vite/client" />
+
+declare module "*.jpg" {
+  const src: string;
+  export default src;
+}
+declare module "*.jpeg" {
+  const src: string;
+  export default src;
+}
+declare module "*.png" {
+  const src: string;
+  export default src;
+}
+declare module "*.gif" {
+  const src: string;
+  export default src;
+}
+declare module "*.svg" {
+  const src: string;
+  export default src;
+}
+declare module "*.mp4" {
+  const src: string;
+  export default src;
+}
+
+interface ImportMetaEnv {
+  readonly VITE_LASTFM_API_KEY?: string;
+  [key: string]: any;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
